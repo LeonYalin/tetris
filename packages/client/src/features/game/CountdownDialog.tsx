6e255585ff96f0aed3381dfbdb227f14a863332f
Leon Yalin <l.yalin@agentvi.com>
2021-09-22 12:41:13 +0300

    fix animation bug

--- packages/client/src/features/game/CountdownDialog.tsx
+++ packages/client/src/features/game/CountdownDialog.tsx
@@ -3,17 +3,26 @@ import { memo, useEffect, useState } from 'react';
 import { createUseStyles } from 'react-jss';
 import { finalize, interval, take, tap } from 'rxjs';
 
+const animDur = 1; // seconds
+
 const useStyles = createUseStyles({
   '@keyframes blinker': {
     from: { opacity: 1, transform: 'scale(1)' },
     to: { opacity: 0, transform: 'scale(2)' },
   },
+  wrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+    width: '300px',
+    height: '300px',
+  },
   textWrapper: {
     color: 'white',
     fontSize: '100px',
-    padding: '50px',
     animationName: '$blinker',
-    animationDuration: '1s',
+    animationDuration: `${animDur}s`,
     animationTimingFunction: 'ease-in-out;',
     animationIterationCount: ({ count }: { count: number }) => `${count}`,
   },
@@ -27,7 +36,7 @@ type Props = {
 };
 
 function CountdownDialog(props: Props) {
-  const { texts = ['3', '2', '1'], switchInterval = 1000, open, onClose: handleClose } = props;
+  const { texts = ['3', '2', '1'], switchInterval = animDur * 1000, open, onClose: handleClose } = props;
   const classes = useStyles({ count: texts.length, theme: undefined });
   const [currText, setCurrText] = useState(texts[0] || '');
   useEffect(() => {
@@ -51,7 +60,9 @@ function CountdownDialog(props: Props) {
         },
       }}
     >
+      <div className={classes.wrapper}>
       <div className={classes.textWrapper}>{currText}</div>
+      </div>
     </Dialog>
   );
 }
